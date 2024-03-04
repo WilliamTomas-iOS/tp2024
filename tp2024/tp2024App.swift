135079041fa7e8b88212d16a50cc2dfd0ac961d8
William Tomas <william.tomas@resilience.care>
2024-03-04 13:55:46 +0100

    Initial Commit

---
+++ tp2024/tp2024App.swift
@@ -0,0 +1,17 @@
+//
+//  tp2024App.swift
+//  tp2024
+//
+//  Created by William Tomas on 04/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct tp2024App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
