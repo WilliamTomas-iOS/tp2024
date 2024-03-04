4b516b2f81ad20621f58b0af93e7b51e9dac3dac
William Tomas <william.tomas@resilience.care>
2024-03-04 16:00:24 +0100

    partie 1 terminée

---
+++ tp2024/Subscription.swift
@@ -0,0 +1,15 @@
+//
+//  Subscription.swift
+//  tp2024
+//
+//  Created by William Tomas on 04/03/2024.
+//
+
+import Foundation
+
+struct Subscription {
+    let id: UUID
+    let name: String
+    let price: Double
+    let startingDate: Date
+}
